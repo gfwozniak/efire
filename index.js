4717d8462970e6da0e5da20450e7e043597fb7c1
Gabriel Wozniak <chungicide@pop-os.localdomain>
2021-04-29 17:59:12 -0500

    fully working discord bot, rate limit compatible, no unhandled promises, now just need to implement deleting bot calls

--- index.js
+++ index.js
@@ -22,12 +22,25 @@ client.on('message', async (message) => {
   if (message.content === 'startfire') {
     // Send "pong" to the same channel
     msg = await message.channel.send("fire starting", 'code');
-    setInterval(p=h=>{
+    let x = 0;
+	inter = setInterval(p=h=>{
 	for(p[I++*I%17+
 		578]=i=89;i++<630;h+=i%30?" *"[p[
 		i]=p[i]+p[i+1]+p[i+29]+p[i+30]>>2
-		]||8:"\n");msg.edit('```' + h + '```', "code");
-    },I=1000)
+		]||8:"\n");
+
+        if (x < 4) {
+                x++;
+        }
+
+        else {
+		msg.edit('```' + h + '\n|\\\\\\\\\\                ///////\n     \\\\\\\\\\      ///////\n          \\\\\\\\\\\\\\\\\n      /////      \\\\\\\\\\\\\\\n /////                  \\\\\\\\\\```', "code");
+                x = 0;
+        }
+
+
+
+    },I=251)
 
   }
   else if (message.content === 'stopfire') {

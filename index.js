54a2c893d6e0c31cc98d76296892a981002a593c
Gabriel Wozniak <chungicide@pop-os.localdomain>
2021-04-28 22:58:09 -0500

    got single time editing of message working

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 
-
-
 const Discord = require('discord.js');
 const Dotenv = require('dotenv').config()
 const client = new Discord.Client();
@@ -9,11 +7,46 @@ client.once('ready', () => {
 	console.log('Brrrrrrrrrrrrrrrrap');
 });
 
-client.on('message', msg => {
-	if (msg.content === 'ping') {
-		msg.reply(msg.type);
-	}
+const config = require('./config.json')
+
+client.on('ready', () => {
+  console.log('The client is ready!');
 });
 
+let msg; //reference to message
+// Create an event listener for messages
+client.on('message', async (message) => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    msg = await message.channel.send("`pong`", 'code');
+    setTimeout(() => {
+	msg.edit('brap');			
+    }, 2000);
 
-client.login(process.env.TOKEN);
+    /*
+    const channel = new Discord.Client().channels.fetch("835921321531015180")
+    channel.messages.fetch({ limit: 1 })
+    */      
+  };
+});
+
+/*
+  client.channels.fetch('835921321531015180').messages.fetch().then((messages) => {
+    if (messages.size === 0) 
+    {
+      // Send a new message
+      channel.send('1')
+    } 
+    else 
+    {
+      // Edit the existing message
+      for (const message of messages) {
+        message[1].edit('2')
+      }
+    }
+  })
+})
+*/
+
+client.login(config.token);

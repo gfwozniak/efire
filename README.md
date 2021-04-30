b1aefe76d27a778643ba59f44af953f12c7c9931
Gabriel Wozniak <chungicide@pop-os.localdomain>
2021-04-30 11:30:27 -0500

    final

---
+++ README.md
@@ -0,0 +1,17 @@
+# efire
+
+This is my first repo, so not exactly sure what info I should put in here...
+
+PACKAGES:
+node.js , discord.js , dotenv
+
+How to use:
+Before anything, you need to add a .env file in the same place index.js is. It should have one line:
+
+TOKEN=[bot token] 
+
+^ without the square brackets
+
+Once you do that you can start the bot using "node index.js". Once it is running, you can use the commands f!start and f!stop (typed out in discord) to start or stop the campfire. As long as the bot has permission to read chat and send messages, it should work.
+
+
--- index.js
+++ index.js
@@ -4,47 +4,41 @@ const Dotenv = require('dotenv').config()
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Brrrrrrrrrrrrrrrrap');
+	console.log('Brrrrrrrrrrrrrrrrap.\nThe client is ready.');
 });
 
-const config = require('./config.json')
-
-client.on('ready', () => {
-  console.log('The client is ready!');
-});
-
-let msg; //reference to message
-let inter; //cancel timeouts and intervals
+let msg; //reference to edited message
+let inter; //interval id so that it can be canceled
+let y = 0; //fire count
 
 // Create an event listener for messages
 client.on('message', async (message) => {
-  // If the message is "ping"
-  if (message.content === 'startfire') {
-    // Send "pong" to the same channel
+  // If the message is "startfire"
+  if (message.content === 'f!start' && y == 0) {
+    y = 1;
+    // Send editable message to the same channel
     msg = await message.channel.send("fire starting", 'code');
     let x = 0;
+	//FÌRE by Martin Kleppe (modified)
 	inter = setInterval(p=h=>{
-	for(p[I++*I%17+
-		578]=i=89;i++<630;h+=i%30?" *"[p[
-		i]=p[i]+p[i+1]+p[i+29]+p[i+30]>>2
-		]||8:"\n");
-
-        if (x < 4) {
+		for(p[I++*I%17+
+			578]=i=89;i++<630;h+=i%30?" *"[p[
+			i]=p[i]+p[i+1]+p[i+29]+p[i+30]>>2
+			]||8:"\n");
+        //this stuff just makes sure the bot uploads at a speed lower than the rate limit
+	if (x < 4) {
                 x++;
         }
-
         else {
 		msg.edit('```' + h + '\n|\\\\\\\\\\                ///////\n     \\\\\\\\\\      ///////\n          \\\\\\\\\\\\\\\\\n      /////      \\\\\\\\\\\\\\\n /////                  \\\\\\\\\\```', "code");
                 x = 0;
         }
-
-
-
-    },I=251)
-
+    },I=300)
   }
-  else if (message.content === 'stopfire') {
+  //breaks interval, deletes message
+  else if (message.content === 'f!stop' && y == 1) {
     clearInterval(inter);
+    y = 0;
     if (msg != undefined) {
 	msg.delete()
 		.then(console.log)
@@ -54,4 +48,4 @@ client.on('message', async (message) => {
 });
 
 
-client.login(config.token);
+client.login(process.env.TOKEN);
